042990a210c04700755df0cda74860d304e83056
AlyElsharkawy <aly.elsharkawy@ejust.edu.eg>
2026-05-05 02:19:26 +0300

    Initial commit

---
+++ src/main/java/org/sportingscout/scout_bank_backend/ScoutBankBackendApplication.java
@@ -0,0 +1,13 @@
+package org.sportingscout.scout_bank_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutBankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sportingscout/scout_bank_backend/ScoutBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sportingscout.scout_bank_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
